7cec72cce6f5a1758ed2ad83adeeaaa04c59b23f
Rohit Mondal <rohitmondal13112001@gmail.com>
2025-08-03 14:09:56 +0530

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const user = false;
+
+  return (
+    <nav className="py-4 flex justify-between items-center">
+      <Link to="/">
+        <img src="/logo.jpeg" className="h-16" alt="" />
+      </Link>
+
+      <div>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>RM</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Rohit</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LinkIcon className="mr-2 h-4 w-4" />
+                My Links
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-400">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Logout </span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
