6290e1c94ab2fc0d662dc7f4666faf959cc70c2c
Rohit Mondal <rohitmondal13112001@gmail.com>
2025-08-03 14:11:54 +0530

    api auth

---
+++ src/db/ApiAuth.ts
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export async function login(email: string, password: string) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) throw new Error(error.message);
+
+  return data
+}
